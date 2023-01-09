2a764d5b6d7cd9117ed2210b329b6129f503f8a1
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-09 12:52:30 -0300

    code per platform (ios or android)

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Text,
   TouchableOpacity,
+  Platform,
   View,
   StyleSheet,
   TouchableNativeFeedback,
@@ -14,21 +15,40 @@ interface Props {
 }
 
 export const FAB = ({title, position = 'br', onPress}: Props) => {
-  return (
-    <View
-      style={[
-        styles.fabLocation,
-        position === 'bl' ? styles.left : styles.right,
-      ]}>
-      <TouchableNativeFeedback
+  const ios = () => {
+    return (
+      <TouchableOpacity
         onPress={onPress}
-        background={TouchableNativeFeedback.Ripple('28425b', false, 30)}>
+        activeOpacity={0.8}
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
         <View style={styles.fab}>
           <Text style={styles.fabText}>{title}</Text>
         </View>
-      </TouchableNativeFeedback>
-    </View>
-  );
+      </TouchableOpacity>
+    );
+  };
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('28425b', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
 };
 
 const styles = StyleSheet.create({
