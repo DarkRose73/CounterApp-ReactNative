3993357fcd61f5fde6ca385830cbfbcc5dc677ee
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-09 11:33:51 -0300

    TouchableOpacity

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(10);
@@ -14,7 +14,11 @@ export const CounterScreen = () => {
         }}>
         Contador: {counter}
       </Text>
-      <Button title="Click" onPress={() => setCounter(counter + 1)} />
+      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+        <View style={{backgroundColor: 'red', borderRadius: 100}}>
+          <Text>+1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
