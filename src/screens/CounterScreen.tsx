9062bbb719ee68c34dfa40eb071557ff80bcb14f
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-09 12:10:16 -0300

    floating action button code

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -6,10 +6,19 @@ export const CounterScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textCounter}>Contador: {counter}</Text>
-      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <View style={styles.buttonAdd}>
-          <Text>+1</Text>
+      <Text style={styles.title}>Contador: {counter}</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setCounter(counter + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setCounter(counter - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -18,10 +27,26 @@ export const CounterScreen = () => {
 
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center'},
-  textCounter: {
+  fab: {
+    backgroundColor: '#5856d6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  // BR -> Bottom Right
+  fabLocationBR: {position: 'absolute', bottom: 25, right: 25},
+  // BL -> Bottom Left
+  fabLocationBL: {position: 'absolute', bottom: 25, left: 25},
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  title: {
     textAlign: 'center',
     fontSize: 40,
     top: -15,
   },
-  buttonAdd: {backgroundColor: 'red', borderRadius: 100},
 });
